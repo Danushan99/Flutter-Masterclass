97fc1f21a2e7f147d506e13bcece82986e69cc05
Danushan <it20051648@my.sliit.lk>
2021-11-09 19:57:18 +0530

    install google font

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,10 +6,22 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset("assets/images/login.png"),
-      ],
-    ));
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

fb58993907cf556e225f63d8523ec68fbbc19fc5
Danushan <it20051648@my.sliit.lk>
2021-11-19 01:02:17 +0530

    round image

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -9,16 +9,17 @@ class MyDrawer extends StatelessWidget {
     const imageUrl = "https://avatars.githubusercontent.com/u/67334286?v=4";
     return Drawer(
       child: ListView(
+        // ignore: prefer_const_literals_to_create_immutables
         children: [
-          DrawerHeader(
+          const DrawerHeader(
               padding: EdgeInsets.zero,
               //margin: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
-                margin: EdgeInsets.zero,
-                accountName: const Text('Dhanu Thazan'),
-                accountEmail: const Text('dhanuthasan08@gmail.com'),
-                currentAccountPicture: Image.network(imageUrl),
-              )),
+                  margin: EdgeInsets.zero,
+                  accountName: Text('Dhanu Thazan'),
+                  accountEmail: Text('dhanuthasan08@gmail.com'),
+                  currentAccountPicture:
+                      CircleAvatar(backgroundImage: NetworkImage(imageUrl)))),
         ],
       ),
     );

9d56f2984606f122079c2d3ef1532bffe42d1b06
Danushan <it20051648@my.sliit.lk>
2021-11-20 00:47:26 +0530

    theem data

---
+++ lib/widget/themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+        primarySwatch: Colors.teal,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        appBarTheme: const AppBarTheme(color: Colors.black),
+        iconTheme: const IconThemeData(color: Colors.black),
+        textTheme: Theme.of(context).textTheme,
+      );
+}

5c25fab70363172c5b61a9bc6413bbf9f0ceda7e
Danushan <it20051648@my.sliit.lk>
2021-11-20 14:08:05 +0530

    add models

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -5,4 +5,22 @@ class Item {
   final num price;
   final String color;
   final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.des,
+      required this.price,
+      required this.color,
+      required this.image});
 }
+
+final products = [
+  Item(
+      id: "prd01",
+      name: "iphone 13",
+      des: "an iphone",
+      price: 99,
+      color: "#33505a",
+      image: "url")
+];
